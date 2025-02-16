aa922c8cf65e722cbfe2e81c80938e47d261e2fc
Chloe <skgus0611skgus@gmail.com>
2025-02-16 10:16:03 -0700

    permission implemented

--- qrscanner/app/(tabs)/index.tsx
+++ qrscanner/app/(tabs)/index.tsx
@@ -1,35 +1,54 @@
-import { View, Text, StyleSheet, SafeAreaView, Pressable, Image, Platform } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Pressable, Linking } from "react-native";
 import { Link, Stack } from "expo-router";
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Camera } from 'expo-camera';
 import { useCameraPermissions } from "expo-camera";
-
 
 export default function HomeScreen() {
   const [permission, requestPermission] = useCameraPermissions();
-
   const isPermissionGranted = Boolean(permission?.granted);
 
-  return (
-    <SafeAreaView
-      style={styles.titleContainer}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Fusion</ThemedText>
-      </ThemedView>
 
-    </SafeAreaView>
+  return (
+    <SafeAreaView style={styles.container}>
+      <Stack.Screen options={{ title: "Overview", headerShown: false }} />
+      <Text style={styles.title}>QR Code Scanner</Text>
+      <View style={{ gap: 20 }}>
+        <Pressable onPress={requestPermission}>
+          <Text style={styles.buttonStyle}>Request Permissions</Text>
+        </Pressable>
+        <Link href={"/(tabs)/camera"} asChild>
+          <Pressable disabled={!isPermissionGranted}>
+            <Text
+              style={[
+                styles.buttonStyle,
+                { opacity: !isPermissionGranted ? 0.5 : 1 },
+              ]}
+            >
+              Scan Code
+            </Text>
+          </Pressable>
+        </Link>
+      </View>
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 160,
-    gap: 8,
-    backgroundColor: '#A1CEDC',
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "black",
+    justifyContent: "space-around",
+    paddingVertical: 80,
+  },
+  buttonStyle: {
+    color: "#0E7AFE",
+    fontSize: 20,
+    textAlign: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 40,
   },
 });
